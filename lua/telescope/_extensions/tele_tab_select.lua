e190aa092f140eb11b22240b84dfdbb5ae9500f3
SamuelTJackson <14303082+SamuelTJackson@users.noreply.github.com>
2021-08-22 00:33:48 +0200

    works

--- lua/telescope/_extensions/tele_tab_select.lua
+++ lua/telescope/_extensions/tele_tab_select.lua
@@ -4,122 +4,44 @@ if not has_telescope then
 end
 
 local vim = vim
+local api = vim.api
 local actions = require('telescope.actions')
 local action_state = require('telescope.actions.state')
 local finders = require('telescope.finders')
 local pickers = require('telescope.pickers')
 local entry_display = require('telescope.pickers.entry_display')
 local sorters = require('telescope.sorters')
-local Previewer = require('telescope.previewers.previewer')
-local icons = require('nvim-web-devicons')
 
-local function goto_window(prompt_bufnr)
+local function goto_line(prompt_bufnr)
     actions.close(prompt_bufnr)
     local entry = action_state.get_selected_entry()
-    vim.api.nvim_set_current_tabpage(entry.tabnr)
-end
-
-local function delete_tab(prompt_bufnr)
-    local entry = action_state.get_selected_entry()
-    local current_picker = action_state.get_current_picker(prompt_bufnr)
-    local windows = vim.api.nvim_tabpage_list_wins(entry.tabnr)
-    for _, windowid in ipairs(windows) do
-        vim.api.nvim_win_close(windowid, false)
-    end
-    current_picker:delete_selection(function(selection)
-    end)
-end
-
-local function get_filename(path)
-    return path:match("([^/]+)$")
-end
-
-local function get_file_extension(path)
-    return path:match("[^.]+$")
+    vim.api.nvim_win_set_cursor(0, {entry.line_number, 0})
 end
 
 local function make_entry()
     local make_display = function(entry)
 
-        local displayer = entry_display.create {separator = "", items = {{width = 10}, {width = 15}}}
-        local tabnr = ""
-        if entry.tabnr == nil then
-            tabnr = "-"
-        else
-            tabnr = entry.tabnr
-        end
-        return displayer {{"Tab: " .. tabnr}, {entry.windows_count .. " window(s)"}}
+        local displayer = entry_display.create {separator = "", items = {{width = 250}}}
+
+        return displayer {{"" .. entry.name}}
     end
 
     return function(entry)
-        local tabidx = ""
-        if entry.tabidx == nil then
-            tabidx = "No Name"
-        else
-            tabidx = entry.tabidx
-        end
-        return {
-            valid = true,
-            path_start = entry.path_start,
-
-            display = make_display,
-            ordinal = "Tab: " .. tabidx .. " : " .. entry.windows_count .. " window(s)",
-            tabnr = entry.tabnr,
-            windows_count = entry.windows_count,
-            windows = entry.windows
-        }
-    end
-end
-
-local function preview_function()
-    return function(_, entry, status)
-        local pr_win = status.preview_win;
-        local bufnr = vim.api.nvim_win_get_buf(pr_win)
-        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, entry.windows)
-    end
-end
-
-local function preview_title()
-    return function()
-        return "Files"
+        return {valid = true, display = make_display, ordinal = entry.name, line_number = entry.line_number, name = entry.name}
     end
 end
 
 local function list(opts)
     opts = opts or {}
+    local buff_number = api.nvim_get_current_buf()
 
-    local tabnrs = vim.api.nvim_list_tabpages()
-    local tabs = {}
+    local buffer = vim.api.nvim_buf_get_lines(buff_number, 0, -1, false)
+    local functions = {}
 
-    for tabidx, tabnr in ipairs(tabnrs) do
-        local windownrs = vim.api.nvim_tabpage_list_wins(tabnr)
-        local tab = {windows_count = 0, windows = {}}
-
-        for windownr, windowid in ipairs(windownrs) do
-
-            local bufnr = vim.api.nvim_win_get_buf(windowid)
-            local buf_name = vim.api.nvim_buf_get_name(bufnr)
-
-            if buf_name ~= "" then
-                tab.tabnr = tabnr
-                tab.tabidx = tabidx
-                tab.windownr = windownr
-                tab.windowid = windowid
-
-                if not string.find(buf_name, "NvimTree") then
-                    tab.windows_count = tab.windows_count + 1
-                    local file_name = get_filename(buf_name)
-                    local file_extension = get_file_extension(buf_name)
-                    local file_icon = icons.get_icon(file_name, file_extension)
-                    if file_icon then
-                        table.insert(tab.windows, file_icon .. " " .. file_name)
-                    else
-                        table.insert(tab.windows, "  " .. file_name)
-                    end
-                end
-            end
+    for i, line in ipairs(buffer) do
+        if string.find(line, "func") then
+            table.insert(functions, {name = line:sub(1, -3), line_number = i})
         end
-        table.insert(tabs, tab)
     end
 
     opts.layout_strategy = "horizontal"
@@ -132,15 +54,12 @@ local function list(opts)
         width = 0.8
     }
     pickers.new(opts, {
-        prompt_title = 'Tabs',
-        finder = finders.new_table {results = tabs, entry_maker = opts.entry_maker or make_entry()},
-        previewer = Previewer:new{title = preview_title(), preview_fn = preview_function()},
+        prompt_title = 'Functions',
+        finder = finders.new_table {results = functions, entry_maker = opts.entry_maker or make_entry()},
         sorter = sorters.get_fzy_sorter(opts),
         attach_mappings = function(_, map)
-            map('i', '<CR>', goto_window)
-            map('n', '<CR>', goto_window)
-            map('n', '<c-d>', delete_tab)
-            map('i', '<c-d>', delete_tab)
+            map('i', '<CR>', goto_line)
+            map('n', '<CR>', goto_line)
             return true
         end
     }):find()
